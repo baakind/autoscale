3fb9cda45c54b9fa51399ba6730b9f3e70ebb3f2
= <=>
2012-09-14 20:28:26 +0200

    Minor changes

--- src/main/java/org/uio/autoscale/Autoscale.java
+++ src/main/java/org/uio/autoscale/Autoscale.java
@@ -2,4 +2,9 @@ package org.uio.autoscale;
 
 public class Autoscale {
 
+	//private static Logger logger = Logger.getLogger(Autoscale.class);
+	
+	public Autoscale() {
+		System.out.println("It works!");
+	}
 }
--- src/test/java/org/uio/autoscale/AutoscaleTest.java
+++ src/test/java/org/uio/autoscale/AutoscaleTest.java
@@ -1,19 +1,26 @@
 package org.uio.autoscale;
 
+import junit.framework.Assert;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 public class AutoscaleTest {
 
-//	@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@AfterClass
-//	public static void tearDownAfterClass() throws Exception {
-//	}
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Autoscale autoscale = new Autoscale();
+		Assert.assertTrue(true); // remove
+	}
 
 }
