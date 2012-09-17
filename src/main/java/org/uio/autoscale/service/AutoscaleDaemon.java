9b5bc4af8b012818c9c2d5cafbaa7f982d5693f3
=Andreas Baakind <=>
2012-09-17 20:18:19 +0200

    Minor changes

--- src/main/java/org/uio/autoscale/service/AutoscaleDaemon.java
+++ src/main/java/org/uio/autoscale/service/AutoscaleDaemon.java
@@ -2,6 +2,9 @@ package org.uio.autoscale.service;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * The runnable instance of Autoscale, for external usage.
  * @author toraba
@@ -10,22 +13,30 @@ import java.io.IOException;
 
 //@Singleton
 public class AutoscaleDaemon implements Runnable {
-	//private static Logger LOG = LoggerFactory.getLogger(AutoscaleDaemon.class);
-
+	private static Logger LOG = LoggerFactory.getLogger(AutoscaleDaemon.class);
     private static final AutoscaleDaemon instance = new AutoscaleDaemon();
     
     
 	
 	public AutoscaleDaemon() {
+		LOG.debug("************** Autoscale Daemon Initialized **********");
+		System.out.println("***************** AUTOSCALE *****************");
 		Thread thread = new Thread(instance);
 		thread.setDaemon(true);
+		thread.setName("Autoscale daemon");
 		thread.start();
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		System.out.println("TEST");
+		LOG.debug("************** Autoscale Daemon Initialized RUN **********");
+		System.out.println("***************** AUTOSCALE RUN *****************");
+		try {
+			Thread.sleep(1000);
+			instance;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		//LOG.debug("Run something here");
 	}
 	
@@ -34,6 +45,7 @@ public class AutoscaleDaemon implements Runnable {
         setup();
     }
 	protected void setup() throws IOException {
-		
+		LOG.debug("************** Autoscale Daemon Initialized SETUP **********");
+		System.out.println("***************** AUTOSCALE SETUP *****************");
 	}
 }
