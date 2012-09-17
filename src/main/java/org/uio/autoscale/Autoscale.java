dbb888754b0b2521851ce17abdf6e7b6bd28756e
Andreas Baakind <andreas@baakind.com>
2012-09-17 10:16:28 +0200

    Initial commit of independent module

--- src/main/java/org/uio/autoscale/Autoscale.java
+++ src/main/java/org/uio/autoscale/Autoscale.java
@@ -1,10 +1,65 @@
 package org.uio.autoscale;
 
-public class Autoscale {
+import java.util.HashSet;
+import java.util.Set;
 
-	//private static Logger logger = Logger.getLogger(Autoscale.class);
+import me.prettyprint.cassandra.connection.HConnectionManager;
+import me.prettyprint.cassandra.service.CassandraClientMonitor;
+import me.prettyprint.cassandra.service.CassandraHost;
+import me.prettyprint.cassandra.service.CassandraHostConfigurator;
+import me.prettyprint.cassandra.service.JmxMonitor;
+/**
+ * Autoscale for cassandra cluster
+ * @author toraba
+ */
+public class Autoscale {
 	
-	public Autoscale() {
-		System.out.println("It works!");
+	private static String CLUSTER_NAME;
+	private CassandraClientMonitor monitor;
+	private Set<CassandraHost> inactiveHosts;
+	
+	public Autoscale(String clusterName) {
+		CLUSTER_NAME = clusterName;
+		
+		CassandraHostConfigurator configurator = new CassandraHostConfigurator();
+		HConnectionManager connectionManager = new HConnectionManager(clusterName, configurator);
+		monitor = JmxMonitor.getInstance().getCassandraMonitor(connectionManager);
+		
+		//CassandraHost host = new CassandraHost("127.0.0.1", 9160);
+		inactiveHosts = new HashSet<CassandraHost>(0);
 	}
+
+	
+	/**
+	 * Initialize new Cassandra-host object in memory
+	 * @param host
+	 * @param port
+	 * @return
+	 */
+	public CassandraHost initNewHost(String host, int port) {
+		return new CassandraHost(host, port);
+	}
+	
+	/**
+	 * Add a host tto cluster
+	 * @param host
+	 * @param port
+	 * @return
+	 */
+	public boolean addHostToCluster(String host, int port) {
+		String hostStr = host + ":" + String.valueOf(port);
+		return monitor.addCassandraHost(hostStr); 
+	}
+	
+	/**
+	 * Remove host from cluster
+	 * @param host
+	 * @param port
+	 * @return
+	 */
+	public boolean removeHostFromCluster(String host, int port) {
+		String hostStr = host + ":" + String.valueOf(port);
+		return monitor.removeCassandraHost(hostStr);
+	}
+
 }
--- src/test/java/org/uio/autoscale/AutoscaleTest.java
+++ src/test/java/org/uio/autoscale/AutoscaleTest.java
@@ -1,26 +1,55 @@
 package org.uio.autoscale;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class AutoscaleTest {
+	private static Autoscale autoscale;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		autoscale = new Autoscale("Test Cluster");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		autoscale = null;
 	}
 
 	@Test
-	public void test() {
-		Autoscale autoscale = new Autoscale();
-		Assert.assertTrue(true); // remove
+	public void testAutoscale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitNewHost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddHostToCluster() {
+		String host = "127.0.0.1";
+		int port = 9160;
+		boolean result;
+		
+		result = autoscale.addHostToCluster(host, port);
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testRemoveHost() {
+		String host = "127.0.0.1";
+		int port = 9160;
+		boolean result;
+		
+		result = autoscale.removeHostFromCluster(host, port);
+		Assert.assertTrue(result);
 	}
 
 }
