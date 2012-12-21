47c0e7a5dc9627f0435817ef8ecbd09072a766b4
Andreas Baakind <toraba@ifi.uio.no>
2012-12-21 13:22:38 +0100

    Linode test

---
+++ src/test/java/no/uio/master/autoscale/AutoscaleLinodeINTTest.java
@@ -0,0 +1,68 @@
+package no.uio.master.autoscale;
+
+import java.util.Set;
+
+import no.uio.master.autoscale.host.CassandraHost;
+import no.uio.master.autoscale.host.CassandraHostManager;
+import no.uio.master.autoscale.host.HostManager;
+import no.uio.master.autoscale.message.AgentMessage;
+import no.uio.master.autoscale.message.enumerator.AgentMessageType;
+import no.uio.master.autoscale.message.enumerator.AgentStatus;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Run test to verify connection to Linode-cluster
+ * @author andreas
+ *
+ */
+public class AutoscaleLinodeINTTest {
+	private static Autoscale scaler;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testStartAutoscaler() throws InterruptedException {
+		Integer intervallTimerAgent = 1;
+		Integer intervallTimerScaler = 10;
+		Integer thresholdBreachLimit = 5;
+		Integer minNumberOfNodes = 1;
+		Double minMemoryUsage = 10.0;
+		Double maxMemoryUsage = 90.0;
+		Long minDiskSpace = 1000L;
+		Long maxDiskSpace = 200000L;
+		String storageLocation = "/";
+		String initHost = "97.107.133.122";
+		Integer initPort = 8002;
+
+		scaler = new Autoscale(intervallTimerAgent, intervallTimerScaler, thresholdBreachLimit,
+				minNumberOfNodes, minMemoryUsage, maxMemoryUsage, minDiskSpace, maxDiskSpace, storageLocation,
+				initHost, initPort);
+		
+		Thread.sleep(10000L);
+		
+		
+	}
+	
+	@Test
+	public void testGetActiveNodes() throws InterruptedException {
+		AgentMessage msg = new AgentMessage(AgentMessageType.STATUS);
+		msg.put("status", AgentStatus.LIVE_NODES);
+		
+		HostManager<CassandraHost> hostManager = new CassandraHostManager();
+		hostManager.updateActiveAndInactiveHosts();
+		Thread.sleep(10000L);
+		Set<CassandraHost> activeHosts = hostManager.getActiveHosts();
+		Assert.assertEquals(2, activeHosts.size());
+	}
+
+}
